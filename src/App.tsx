f5f4f2dc08cd4126ec4f4114ef3c8ec0046feff9
경원 <121841669+deswaq1220@users.noreply.github.com>
2025-06-22 17:21:23 +0900

    🩹 경로 수정 및 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import './App.css';
-import AuthInput from './components/auth/AuthInput';
-import Button from './components/common/Button';
-import Input from './components/common/Input';
-import ToastMessage from './components/common/ToastMessage';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 function App() {
-  return (
-    <>
-      <AuthInput
-        type='text'
-        label='인증번호'
-        placeholder='인증번호를 입력하세요'
-      />
-      <Button variant='cancel'>취소</Button>
-      <Button>로그인</Button>
-      <Button className='bg-sub-green rounded-full'>선택완료</Button>
-      <Input />
-      <ToastMessage message='테스ㅡㅌ' />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
+
 export default App;
