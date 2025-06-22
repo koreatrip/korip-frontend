bac8071ece89df0ebe07cd87fb4e6840e44aaf06
경원 <121841669+deswaq1220@users.noreply.github.com>
2025-06-22 14:05:19 +0900

    💄 style: authInput 컴포넌트 생성 #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import AuthInput from './components/auth/AuthInput';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-2xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AuthInput
+        type='text'
+        label='인증번호'
+        placeholder='인증번호를 입력하세요'
+      />
     </>
   );
 }
-
 export default App;
