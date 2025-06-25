25c903c1b4ba11532a9a21b8f8b79e7a726e23be
taeyuls <startez03@gmail.com>
2025-06-25 22:27:35 +0900

    feat: 마이페이지 라우팅 추가

---
+++ src/pages/myPage/index.tsx
@@ -0,0 +1,9 @@
+const MyPage = () => {
+  return (
+    <main className='mx-auto max-w-4xl p-6'>
+      <h1 className='mb-4 text-2xl font-bold'>My Page</h1>
+    </main>
+  );
+};
+
+export default MyPage;
