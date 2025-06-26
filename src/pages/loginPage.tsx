3350df8f2a8ae2a59fed334377af8a63d9ab7273
sonsodockje <hanaroo0115@naver.com>
2025-06-26 16:38:35 +0900

    💄Style: 로그인 페이지 컴포넌트 제작 및 배치 #6

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import HorizonLine from '@/components/common/HorizonLine';
+import LoginForm from '@/components/login/LoginForm';
+import WelcomeCard from '@/components/login/WelcomeCard';
+import SocialLoginButtons from '@/components/login/SocialLoginButtons';
 
 const LoginPage = () => {
-  return <div>loginPage</div>;
+  return (
+    <div className='w-full flex-col items-center justify-center p-8'>
+      <WelcomeCard
+        accountQuestionText="Don't have an account?"
+        linkText='Sign Up'
+        linkHref='/signup'
+      />
+      <LoginForm />
+      <HorizonLine text='Or continue with  ' />
+      <SocialLoginButtons />
+    </div>
+  );
 };
 
 export default LoginPage;
