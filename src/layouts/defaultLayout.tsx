0dca4d5d24daff5f9035d4fb348654210edc398d
sonsodockje <hanaroo0115@naver.com>
2025-06-26 22:05:19 +0900

    💄Style: 공통 레이아웃에 헤더 추가

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/header/Header';
 import { Outlet } from 'react-router';
 
 const defaultLayout = () => {
   return (
     <div className='bg-bg-white text-main-text-navy font-pretendard flex w-full items-center justify-center'>
       <div className='w-[1440px]'>
+        <Header />
         <Outlet />
       </div>
     </div>
