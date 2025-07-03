2d8f598a742dd8518d85bea710574a448519eebe
경원 <121841669+deswaq1220@users.noreply.github.com>
2025-07-03 17:41:47 +0900

    style: 탭 크기 고정

--- src/components/common/Tab.tsx
+++ src/components/common/Tab.tsx
@@ -10,7 +10,7 @@ const Tab = ({
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 font-semibold transition-colors duration-200 ${
+      className={`flex-shrink-0 px-4 py-2 font-semibold transition-colors duration-200 ${
         isActive
           ? 'border-sub-green text-sub-green border-b-2'
           : 'text-main-text-navy/90 hover:text-main-text-navy'
