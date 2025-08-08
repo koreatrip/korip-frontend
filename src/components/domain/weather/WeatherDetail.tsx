0f89fea763de27691b8d97919e7abc40a7396b22
경원 <121841669+deswaq1220@users.noreply.github.com>
2025-08-08 12:19:12 +0900

    💄 style: 지역둘러보기 반응형 완료 #54

--- src/components/domain/weather/WeatherDetail.tsx
+++ src/components/domain/weather/WeatherDetail.tsx
@@ -1,14 +1,15 @@
-// src/components/domain/weather/WeatherDetail.tsx
 type WeatherDetailProps = {
   label: string;
   value: string;
 };
+
 const WeatherDetail = ({ label, value }: WeatherDetailProps) => {
   return (
-    <div className='bg-bg-section border-outline-gray flex w-full flex-col items-center justify-center rounded-2xl border px-8 py-4'>
-      <p className='text-sub-text-gray'>{label}</p>
-      <p className='text-[20px] font-medium'>{value}</p>
+    <div className='bg-bg-section border-outline-gray tablet-bp:px-8 tablet-bp:py-4 tablet-bp:min-w-0 flex w-full min-w-[120px] flex-col items-center justify-center rounded-2xl border px-4 py-3'>
+      <p className='text-sub-text-gray tablet-bp:text-base text-sm'>{label}</p>
+      <p className='tablet-bp:text-[20px] text-lg font-medium'>{value}</p>
     </div>
   );
 };
+
 export default WeatherDetail;
