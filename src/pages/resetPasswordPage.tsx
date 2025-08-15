d0d9722f6bb77e4c7dcd047b7c1744efead240d1
경원 <121841669+deswaq1220@users.noreply.github.com>
2025-08-15 19:40:55 +0900

    🌐 번역
    #68

--- src/pages/resetPasswordPage.tsx
+++ src/pages/resetPasswordPage.tsx
@@ -22,21 +22,21 @@ const ResetPasswordPage = () => {
 
         {/* 메인 헤딩 */}
         <h2 className='text-main-text-navy mb-2 text-2xl font-bold'>
-          임시 비밀번호가 발급되었습니다.
+          {t('common.temp_password_issued')}
         </h2>
 
         {/* 설명 텍스트 */}
         <p className='text-sub-text-gray mb-2'>
-          입력하신 이메일로 임시 비밀번호를 발송했습니다.
+          {t('common.temp_password_sent')}
         </p>
         <p className='text-sub-text-gray mb-8'>
-          로그인 후 새로운 비밀번호로 변경해주세요.
+          {t('common.change_after_login')}
         </p>
 
         {/* 추가 안내 텍스트 */}
         <div className='text-main-pink mb-8 rounded-lg p-4 text-sm'>
-          <p className='mb-1 font-bold'>이메일을 받지 못하셨나요?</p>
-          <p>스팸함을 확인하거나 몇 분 후 다시 시도해주세요.</p>
+          <p className='mb-1 font-bold'> {t('common.email_not_received')}</p>
+          <p>{t('common.check_spam_or_retry')}</p>
         </div>
 
         {/* 로그인 버튼 */}
