ddb5578f5de42509218ec181b8177adbf901c568
sonsodockje <hanaroo0115@naver.com>
2025-09-21 18:47:49 +0900

    ✨ feat: 지역 둘리보기 네비게이션 연결

--- src/components/domain/regions/FirstInfoCard.tsx
+++ src/components/domain/regions/FirstInfoCard.tsx
@@ -1,22 +1,36 @@
 import star from '@/assets/star/star.svg';
+import { useToast } from '@/hooks/useToast';
+import { useNavigate } from 'react-router';
 
 type TFirstCard = {
-  variant: 'interactive' | 'selectable';
+  isRegion: boolean;
+  id: number;
+  // variant: 'interactive' | 'selectable';
   title: string;
   imageUrl: string | null;
   isSelected: boolean;
-  onClick: () => void;
-  onViewDetails: () => void;
-  onFavorite: () => void;
 };
 
 const FirstInfoCard = ({
   // variant = 'interactive', // 'interactive' | 'selectable'
+  id,
+  isRegion,
   title,
   imageUrl,
-  onViewDetails = () => {},
-  onFavorite = () => {},
 }: TFirstCard) => {
+  const { showToast } = useToast();
+
+  function handleClickDetail() {
+    // console.log('View details for item with id:', id);
+    if (isRegion) {
+      navigator(`explore/regions?region_id=${id}`);
+    } else {
+      // navigator(`explore/attractions?category_id=${id}`);
+      showToast('준비 중인 기능입니다.', 'info');
+    }
+  }
+
+  const navigator = useNavigate();
   return (
     <div>
       <div
@@ -25,21 +39,21 @@ const FirstInfoCard = ({
           backgroundImage: `url(${imageUrl || 'https://via.placeholder.com/300x200'})`,
         }}
       >
-        <button
-          onClick={onFavorite}
-          className='absolute right-3 bottom-3 z-30 cursor-pointer'
-        >
-          <img src={star} alt='star' />
-        </button>
+        {isRegion
+          ? // <button className='absolute right-3 bottom-3 z-30 cursor-pointer'>
+            //   <img src={star} alt='star' />
+            // </button>
+            null
+          : null}
       </div>
 
-      <div className='flex items-center justify-between py-5'>
-        <h3 className='text-main-text-navy text-2xl font-semibold lg:text-3xl'>
+      <div className='flex items-center justify-between px-2 py-2 md:py-5'>
+        <h3 className='text-main-text-navy text-xl font-semibold md:text-2xl'>
           {title}
         </h3>
         <button
-          onClick={onViewDetails}
-          className='text-main-text-navy bg-bg-white border-outline-gray shadow-light cursor-pointer rounded-full border px-3 py-2 text-sm font-medium duration-300 hover:bg-gray-200 md:text-xl lg:px-5'
+          onClick={() => handleClickDetail()}
+          className='text-main-text-navy bg-bg-white border-outline-gray shadow-light md:text-md cursor-pointer rounded-full border px-3 py-2 text-sm font-medium whitespace-nowrap duration-300 hover:bg-gray-200 lg:px-5'
         >
           둘러보기
         </button>
