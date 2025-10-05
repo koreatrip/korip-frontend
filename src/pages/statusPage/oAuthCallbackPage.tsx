207b3f9fb085826cdbb75a18bb82fc479a16b5c8
경원 <121841669+deswaq1220@users.noreply.github.com>
2025-10-05 15:00:56 +0900

    🩹 chore/ 타입수정

--- src/pages/statusPage/oAuthCallbackPage.tsx
+++ src/pages/statusPage/oAuthCallbackPage.tsx
@@ -51,20 +51,21 @@ const OAuthCallbackPage = () => {
           ? '/language'
           : '/first-region-search';
         navigate(redirectTo);
-      } catch (error) {
-        const errorMessage =
-          error instanceof AxiosError
-            ? error.response?.data?.message || error.message
-            : error.message || '로그인을 실패하였습니다.';
+      } catch (err: unknown) {
+        let errorMessage = '로그인을 실패하였습니다.';
+
+        if (err instanceof AxiosError) {
+          errorMessage = err.response?.data?.message || err.message;
+        } else if (err instanceof Error) {
+          errorMessage = err.message;
+        }
 
         showToast(errorMessage, 'error');
-
         window.history.replaceState(
           {},
           document.title,
           window.location.pathname
         );
-
         setStatus('error');
       }
     };
