57613769e52f0c89d9701f64b6db44ace4d29313
경원 <121841669+deswaq1220@users.noreply.github.com>
2026-02-05 00:18:32 +0900

    ♻️확장자 수정

--- src/pages/languagePage.tsx
+++ src/pages/languagePage.tsx
@@ -1,5 +1,5 @@
 import Container from '@/components/common/Container';
-import bg from '@assets/lagnage_bg.png';
+import bg from '@assets/lagnage_bg.webp';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
