38487a3494d76d082e839e9047c42b7e4486907c
경원 <121841669+deswaq1220@users.noreply.github.com>
2026-02-04 23:58:09 +0900

    이미지 최적화

--- src/pages/languagePage.tsx
+++ src/pages/languagePage.tsx
@@ -1,5 +1,4 @@
 import Container from '@/components/common/Container';
-import bgWebp from '@assets/lagnage_bg.webp';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -57,15 +56,12 @@ const LanguagePage = () => {
         //   backgroundRepeat: 'no-repeat',
         // }}
       >
-        <picture className='absolute inset-0 z-0 h-full w-full'>
-          <source srcSet={bgWebp} type='image/webp' />
-          <img
-            src='/lagnage_bg.webp'
-            alt='언어선택 배경 이미지'
-            className='h-full w-full object-cover'
-            fetchPriority='high'
-          />
-        </picture>
+        <img
+          src='/lagnage_bg.webp'
+          alt='언어선택 배경 이미지'
+          className='absolute inset-0 z-0 h-full w-full object-cover'
+          fetchPriority='high'
+        />
         <div className='absolute top-0 left-0 h-full w-full bg-black opacity-30'></div>
         <p className='text-bg-white relative z-10 text-5xl'>
           {t('auth.welcome_message_title')}
